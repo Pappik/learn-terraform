13f567ddb7dacd49a60e912542bb1a9db330d6ee
pappik <pappik@gmail.com>
2023-12-07 13:31:13 -0500

    jenkins

--- resources/ec2/ec2.tf
+++ resources/ec2/ec2.tf
@@ -1,5 +1,5 @@
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.centos8
+resource "aws_instance" "centos8" {
+  ami           = data.aws_ami.centos8.id
   instance_type = "t3.micro"
   vpc_security_group_ids = [var.security_group_id]
 
